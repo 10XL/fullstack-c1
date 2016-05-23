b48635721bc40a354d5c40238a7ea509cc06b438
10xl <urnotuurmi@gmail.com>
2016-05-22 20:16:52 -0400

    fix syntax error

--- fullstack-c4/module1/ionic/conFusion/www/js/app.js
+++ fullstack-c4/module1/ionic/conFusion/www/js/app.js
@@ -82,3 +82,4 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
 
+});
