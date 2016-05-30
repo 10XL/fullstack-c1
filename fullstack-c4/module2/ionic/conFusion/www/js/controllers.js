9dd4021bd6b45a33d1a02b622905cb4bd2b48655
10xl <urnotuurmi@gmail.com>
2016-05-29 21:09:35 -0400

    use $localStorage

--- fullstack-c4/module2/ionic/conFusion/www/js/controllers.js
+++ fullstack-c4/module2/ionic/conFusion/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('conFusion.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -13,6 +13,12 @@ angular.module('conFusion.controllers', [])
   $scope.loginData = {};
   $scope.reservation = {};
 
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+}).then(function(modal) {
+    $scope.loginform = modal;
+});
+
   // Create the reserve modal that we will use later
   $ionicModal.fromTemplateUrl('templates/reserve.html', {
     scope: $scope
@@ -22,17 +28,18 @@ angular.module('conFusion.controllers', [])
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+    $scope.loginform.hide();
 };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+    $scope.loginform.show();
 };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
+    $localStorage.storeObject('userinfo',$scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
