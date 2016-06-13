21a8c9cabfe7c33766b886fc56204b76f6718146
10xl <urnotuurmi@gmail.com>
2016-06-13 05:06:11 -0400

    fix id name

--- fullstack-c5/module1/node-express/promoRouter.js
+++ fullstack-c5/module1/node-express/promoRouter.js
@@ -24,22 +24,22 @@ promoRouter.route('/')
         res.end('Deleting all promotions');
 });
 
-promoRouter.route('/:dishId')
+promoRouter.route('/:promoId')
 .all(function(req,res,next) {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       next();
 })
 
 .get(function(req,res,next){
-        res.end('Will send details of the promotion: ' + req.params.dishId +' to you!');
+        res.end('Will send details of the promotion: ' + req.params.promoId +' to you!');
 })
 
 .put(function(req, res, next){
-        res.write('Updating the promotion: ' + req.params.dishId + '\n');
+        res.write('Updating the promotion: ' + req.params.promoId + '\n');
     res.end('Will update the promotion: ' + req.body.name + 
             ' with details: ' + req.body.description);
 })
 
 .delete(function(req, res, next){
-        res.end('Deleting promotion: ' + req.params.dishId);
+        res.end('Deleting promotion: ' + req.params.promoId);
 });
