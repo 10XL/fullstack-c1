07f4088b57f8e1bc4d967978f4cf43370945c035
10xl <urnotuurmi@gmail.com>
2016-06-17 14:11:52 -0400

    remove currency type

--- fullstack-c5/module2/node-mongoose/leadership.js
+++ fullstack-c5/module2/node-mongoose/leadership.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-require('mongoose-currency').loadType(mongoose);
-var Currency = mongoose.Types.Currency;
-
 var leaderSchema = new Schema({
     name: {
         type: String,
