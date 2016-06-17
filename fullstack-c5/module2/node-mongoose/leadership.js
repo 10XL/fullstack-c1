81a130708eb36329d137bdca0b08c8fa85f0aab8
10xl <urnotuurmi@gmail.com>
2016-06-17 14:07:22 -0400

    create schemas

---
+++ fullstack-c5/module2/node-mongoose/leadership.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,  
+    },
+    designation: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    abbr: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var Leaderships = mongoose.model('Leadership', leaderSchema);
+module.exports = Leaderships;
