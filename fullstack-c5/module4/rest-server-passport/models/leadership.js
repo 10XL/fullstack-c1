c753a8ab2e49d38b879a8a5840211fbc7833a17d
10xl <urnotuurmi@gmail.com>
2016-06-27 21:46:20 -0400

    setup module4

---
+++ fullstack-c5/module4/rest-server-passport/models/leadership.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,  
+    },
+    designation: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    abbr: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var Leaderships = mongoose.model('Leadership', leaderSchema);
+module.exports = Leaderships;
